74e0f56766acfde53f9bd9493dbdcbde3288c6eb
R.M.CHAK <odooerpcustomize@gmail.com>
2019-03-03 16:06:26 +0530

    [ADD] module odoo 10

---
+++ serach_by_variants/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Search By Variants and Product',
+    'version' : '1.0',
+    'summary': '',
+
+    'category': 'Product',
+    'website': '',
+    'author' : 'We-R Informative'
+    'depends' : ['base','sale','product','web'],
+    'data': [
+             'views/from_common.xml',
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
